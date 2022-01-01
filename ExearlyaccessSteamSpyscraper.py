37b1d6b7d39f38c6fc82790a935345b887cfb5c6
Renee <79910937+ReneeN1998@users.noreply.github.com>
2022-01-01 19:34:50 +0100

    Add files via upload

---
+++ ExearlyaccessSteamSpyscraper.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests 
+import pandas as pd
+
+url = 'https://steamspy.com/genre/Ex+Early+Access'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+  
+data = []
+table = soup.find('table', attrs={'id':'gamesbygenre'})
+table_body = table.find('tbody')
+rows = table_body.find_all('tr')
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele]) # Get rid of empty values
+    
+df = pd.DataFrame(data)
+df.columns=['#', 'title', 'releasedate', 'price', 'Score_rank', 'owners', 'players', 'playtime_median', 'developers', 'publishers' ]
+
+df.to_csv('../Thesis/exearlyaccesssteamspy.csv', index = False, encoding='utf-8')
+print('Succesfully saved!') 
